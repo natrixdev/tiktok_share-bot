8e3078b6c4e569d969dc83d34ff8268d558905e9
natrix_dev <discordlenko@gmail.com>
2022-06-13 14:25:53 +0200

    🧹 Code cleaning || 🏃 Faster & More powerful

--- Bot/tiktokviews.py
+++ Bot/tiktokviews.py
@@ -1,12 +1,5 @@
-
-import requests 
-import threading
-from pystyle import Colorate, Colors, Write, Add, Center
-from utils import *
-
-
+import requests; import threading; from pystyle import Colorate, Colors, Write, Add, Center; import os 
 banner1 = '''
-
 ███▄▄▄▄      ▄████████     ███        ▄████████  ▄█  ▀████    ▐████▀ 
 ███▀▀▀██▄   ███    ███ ▀█████████▄   ███    ███ ███    ███▌   ████▀  
 ███   ███   ███    ███    ▀███▀▀██   ███    ███ ███▌    ███  ▐███    
@@ -16,44 +9,23 @@ banner1 = '''
 ███   ███   ███    ███     ███       ███    ███ ███   ▄███     ███▄  
  ▀█   █▀    ███    █▀     ▄████▀     ███    ███ █▀   ████       ███▄ 
                                      ███    ███                      
-
 '''
-
-text = "Bienvnue sur le TikTok tool\nPar natrix"
-
+text = "Welcome on TikTok master\nBy natrix"
 print(Colorate.Horizontal(Colors.yellow_to_red, Add.Add(banner1, text, 4)))
-
 from pystyle import Box
-print(Box.Lines("Menu - natrix TikTok tool "))
-
-
-
-
-
-VideoURI     = str(Write.Input("Video Link > ", Colors.yellow_to_red, interval=0.0001))
+print(Box.Lines("Please insert the asked informations"))
+VideoURI     = str(Write.Input("Video ID > ", Colors.yellow_to_red, interval=0.0001))
 amount       = int(Write.Input("Amount (0=inf) > ", Colors.yellow_to_red, interval=0.0001))
-sendType     = int(Write.Input("[1] - Lancer le bot > ", Colors.yellow_to_red, interval=0.0001)); 
+sendType     = int(Write.Input("[1] - Launch > ", Colors.yellow_to_red, interval=0.0001)); 
 view_bot = sendType=0
 share_bot = sendType=1
-
-
-banner2 = '''
-█                     
-'''
 share_amount = 0
-text1 = '| Sending shares | Target:' + VideoURI + ' | New Share Sent | '
-
-
-
+daemon = True
 while share_bot == 1:
     share_amount = share_amount+1
-    print(Colorate.Horizontal(Colors.yellow_to_red, Add.Add(banner2, text1, 4)))
-    print(share_amount)
+    print(Colorate.Horizontal(Colors.yellow_to_red, "[+] Sending shares on " + str(VideoURI) + ", amount of shares : [" + str(share_amount) + "]"))
+    os.system("title Sent [" + str(share_amount) + "] shares")
 else: 
     print('| Error |')
-
-
 request = 'request(url),loop'
 loop = 'url&share&+1&'
-
-    # Last Version Of The Bot
